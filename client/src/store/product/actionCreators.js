008bb4d27bace671315e83f168337cd7676faf7d
StepanMogilyov <st.mogilev@mail.ru>
2022-12-06 13:39:14 +0300

    store in prog 4

--- client/src/store/product/actionCreators.js
+++ client/src/store/product/actionCreators.js
@@ -1,6 +1,5 @@
 // import { GET_USER, LOGOUT_USER } from './actionTypes';
 
 export function getProductAC(newProduct) {
-  console.log('newProduct: ', newProduct);
   return { type: 'GET_PRODUCT', payload: newProduct };
 }
--- client/src/store/product/productReducer.js
+++ client/src/store/product/productReducer.js
@@ -6,7 +6,7 @@ export default function productReducer(state = initialState, action) {
 
   switch (action.type) {
     case 'GET_PRODUCT': {
-      return [state, ...action.payload]
+      return [...state, action.payload]
     }
     default:
       return state;
