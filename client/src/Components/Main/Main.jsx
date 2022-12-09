2be4e822e66f5826b54b6804d2c1d1874aa446b7
StepanMogilyov <st.mogilev@mail.ru>
2022-12-09 12:47:35 +0300

    show gratitude done

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -5,17 +5,23 @@ import styles from "./Main.module.css";
 import BarcodeScanner from "../Scanner/BarcodeScanner";
 import { useDispatch, useSelector } from "react-redux";
 import { resetProductsAC } from "../../store/product/actionCreators";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 
 export default function Main() {
   const [sum, setSum] = useState(0);
   const [rerender, setRerender] = useState(0);
+  const [showSmile, setShowSmile] = useState(false);
   const allProducts = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
-  const resetStateHandler = () => {
+  const payBtnHandler = () => {
     dispatch(resetProductsAC());
     setSum(0);
     setRerender(rerender + 1);
+    setShowSmile(true);
+    setTimeout(() => {
+      setShowSmile(false);
+    }, 3500);
   };
 
   const addSumHandler = (price) => {
@@ -29,6 +35,17 @@ export default function Main() {
   return (
     <div className={styles.main}>
       <BarcodeScanner rerender={rerender} />
+
+      {showSmile ? (
+        <><InsertEmoticonIcon
+          style={{ height: "110px", width: "110px", color: "green", marginLeft: '100px' }}
+        />
+        <h1 style={{color: 'green', marginLeft: '20px'}}>Спасибо за покупку</h1>
+        </>
+      ) : (
+        <></>
+      )}
+
       {allProducts.length ? (
         <div className={styles.products}>
           <div className={styles.sum}>Сумма покупки: {sum}</div>
@@ -42,7 +59,7 @@ export default function Main() {
           ))}
           <div className={styles.payBtn}>
             <Button
-              onClick={resetStateHandler}
+              onClick={payBtnHandler}
               type="submit"
               variant="contained"
               color="success"
